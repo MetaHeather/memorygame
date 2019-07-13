a23116f3eeae45c2b9658b891bbb26639138a3c6
Heather Etheridge <heathernetheridge@gmail.com>
2019-07-13 17:43:17 -0500

    created game logic

---
+++ js/main.js
@@ -0,0 +1,10 @@
+console.log("Up and running!");
+//Variables to hold card values
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree =  "king";
+var cardFour = "king";
+
+//log 2 cards 
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
