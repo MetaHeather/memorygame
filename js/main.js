9b0c4cb502706bf4b4a91ac61be7ae11eeb6f3ee
Heather Etheridge <heathernetheridge@gmail.com>
2019-07-16 18:27:47 -0500

    created logic for flipping cards

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
-console.log("Up and running!");
-
 //array to hold cards
 const cards = ["queen", "king", "queen", "king"];
 //empty array to hold cards that get flipped over
-const cardsInPlay = [];
-//variable to hold first card that user flips
-const cardOne = cards[0];
-//push the card that the user flipped into the cardsInPlay array
-cardsInPlay.push(cardOne);
-//console log to verify card flipped added to cardsInPlay
-console.log("User flipped " + cardsInPlay[0]);
-//variable to hold second card flipped
-const cardTwo = cards[1];
-//push the card that the user flipped into cardsInPlay
-cardsInPlay.push(cardTwo);
-//console log to verify card flipped added to cardsInPlay
-console.log("User flipped " + cardsInPlay[1]);
-//statement to check if two cards have been played to check for a match
-if(cardsInPlay.length === 2){
+let cardsInPlay = [];
+//function to hold loic for card flips
+function flipCard(cardId){
+    //console log to show which card user flipped
+    console.log("User flipped " + cards[cardId]);
+    //add the flipped card to cardsInPlay
+    cardsInPlay.push(cards[cardId]);
+    //statement to check if two cards have been played to check for a match
+    if(cardsInPlay.length === 2){
+      checkForMatch();
+    }
+}   
+//function to check cards in cardsInPlay for a match
+function checkForMatch(){
   if(cardsInPlay[0] === cardsInPlay[1]){
-    alert("You found a match!");
+      alert("You found a match!");
   }else{
       alert("Sorry, try again.");
   }
 }
+//Call the flip card function
+flipCard(0);
+flipCard(2);
